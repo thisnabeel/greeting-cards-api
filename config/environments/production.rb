068157b39b5aa39731844ba8806fdafde329dac4
Nabeel Khan <nabeel.khan.swe@gmail.com>
2026-03-19 10:28:56 -0700

    api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,10 +74,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  # Whitelist Railway preview/prod domains.
+  config.hosts << "greeting-cards-api-production.up.railway.app"
+  config.hosts << /.*\.up\.railway\.app/
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
